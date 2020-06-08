f27eaf0c6b6831e02b67c1391c98f9bce33ddf5a
KC <krkeco@gmail.com>
2020-06-08 09:11:35 -0700

    add files

---
+++ src/components/join/JoinButton.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+import { URL, playerTypeEnum } from '../../constants';
+
+const JoinButton = ({
+  awake,
+  createGame,
+  playerType,
+  joinGame,
+  startGame,
+  gameId,
+}) => {
+  if (!awake) {
+    return <div className="loader"> </div>;
+  }
+
+  if (playerType == playerTypeEnum.GUEST) {
+    return (
+      <Button
+        disabled={gameId > -1 ? true : false}
+        style={{ margin: 5 }}
+        onClick={joinGame}
+        variant="contained"
+        color="secondary"
+      >
+        Join Game
+      </Button>
+    );
+  }
+
+  if (
+    (playerType == playerTypeEnum.HOST || playerType == playerTypeEnum.LAN) &&
+    gameId >= 0
+  ) {
+    return (
+      <Button
+        style={{ margin: 5 }}
+        onClick={startGame}
+        variant="contained"
+        color="primary"
+      >
+        Start Game {gameId}
+      </Button>
+    );
+  }
+
+  return (
+    <Button
+      style={{ margin: 5 }}
+      onClick={createGame}
+      variant="contained"
+      color="secondary"
+    >
+      Create Game
+    </Button>
+  );
+};
+
+export default JoinButton;
